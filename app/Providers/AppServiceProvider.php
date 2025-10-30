c95d96f4e6842ba4f9e51f01e94f7447bc723610
muftioliullahnomani <126228907+muftioliullahnomani@users.noreply.github.com>
2025-10-30 07:37:44 +0600

    K

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\Facades\Vite;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -21,5 +22,12 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Vite::prefetch(concurrency: 3);
+        if (app()->environment('production')) {
+            $appUrl = config('app.url');
+            if ($appUrl) {
+                URL::forceRootUrl($appUrl);
+            }
+            URL::forceScheme('https');
+        }
     }
 }
